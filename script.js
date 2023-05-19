fce0795dca8d84b1b67673f72b2a8f20f69daa38
Kevin Li <kevinli617@gmail.com>
2023-05-19 19:20:12 -0400

    refactored functions to reduce repetition.

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-// Retrieve API key
 let apiKey = 'b2aaa7561cc145a5b412d187b054ba79'
 
 let $navList = $('#nav-list')
 let $greenBtn = $('#green-btn')
+let $redBtn = $('#red-btn')
+let $orangeBtn = $('#orange-btn')
+let $blueBtn = $('#blue-btn')
+
 let $trainList = $('#train-list')
+let $leftContainer = $('.left-container')
 let $rightContainer = $('.right-container')
 
-$greenBtn.on('click', async function() {
-    let response = await axios.get(`https://api-v3.mbta.com/vehicles?api_key=b2aaa7561cc145a5b412d187b054ba79&filter[route]='Green-B,Green-C,Green-D,Green-E'`)
+$navList.on('click', 'button', async function() {
+    $trainList.empty()
+    let hex = $(this).attr("hex")
+    console.log(hex)
+    $leftContainer.css("border", `3px solid ${hex}`)
+    let value = $(this).prop("value")
+    let routeFilter = ''
+    switch (value) {
+        case 'green':
+            routeFilter = 'Green-B,Green-C,Green-D,Green-E'
+            break
+        case 'red':
+            routeFilter = 'Red'
+            break
+        case 'orange':
+            routeFilter = 'Orange'
+            break
+        case 'blue':
+            routeFilter = 'Blue'
+            break
+    } 
+    let response = await axios.get(`https://api-v3.mbta.com/vehicles?api_key=${apiKey}&filter[route]=${routeFilter}`)
     let data = response.data.data
     for (const train of data) {
         let id = train.id
@@ -29,18 +53,18 @@ $greenBtn.on('click', async function() {
             default:
                 return
         }
-        let stop = await getStop(train.relationships.stop.data.id)        
+        let stop = await getStop(train.relationships.stop.data.id)
         $trainList.append(`
         <li>
-            <button class="train-btn green" id="${id}" lat="${lat}" lon="${lon}">
-                ${id}</br>${route}</br>${lat}, ${lon}</br>${status}</br>${stop}
+            <button class="train-btn ${value}" id="${id}" lat="${lat}" lon="${lon}">
+                ${id}</br>${route}</br>${status}</br>${stop}
             </button>
         </li>
         `)
     }
 })
 
-$trainList.on('click', 'button', async function() {
+$trainList.on('click', 'button', function() {
     let id = $(this).prop("id")
     let lat = $(this).attr("lat")
     let lon = $(this).attr("lon")
