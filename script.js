0bbc3b61e8927500e68ca4e87d2616e0936fdba2
Kevin Li <kevinli617@gmail.com>
2023-05-19 18:15:02 -0400

    map rendering for green line trains

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 // Retrieve API key
-let apiKey = '';
-(async function () {
-    const response = await fetch('./api.key')
-    const text = await response.text()
-    apiKey = text
-})()
+let apiKey = 'b2aaa7561cc145a5b412d187b054ba79'
 
 let $navList = $('#nav-list')
 let $greenBtn = $('#green-btn')
 let $trainList = $('#train-list')
+let $rightContainer = $('.right-container')
 
 $greenBtn.on('click', async function() {
     let response = await axios.get(`https://api-v3.mbta.com/vehicles?api_key=b2aaa7561cc145a5b412d187b054ba79&filter[route]='Green-B,Green-C,Green-D,Green-E'`)
     let data = response.data.data
-    console.log(data)
     for (const train of data) {
         let id = train.id
         let route = train.relationships.route.data.id
         let lat = train.attributes.latitude
         let lon = train.attributes.longitude
-
         let status = ''
         let currentStatus = train.attributes.current_status
         switch (currentStatus) {
@@ -35,10 +29,7 @@ $greenBtn.on('click', async function() {
             default:
                 return
         }
-
-        let stop = await getStop(train.relationships.stop.data.id)
-        // console.log(train.relationships.stop.data.id)
-        
+        let stop = await getStop(train.relationships.stop.data.id)        
         $trainList.append(`
         <li>
             <button class="train-btn green" id="${id}" lat="${lat}" lon="${lon}">
@@ -49,13 +40,12 @@ $greenBtn.on('click', async function() {
     }
 })
 
-$trainList.on('click', 'button', function() {
+$trainList.on('click', 'button', async function() {
     let id = $(this).prop("id")
     let lat = $(this).attr("lat")
     let lon = $(this).attr("lon")
-    console.log(id, lat, lon)
+    $rightContainer.html(`<img src="https://www.mapquestapi.com/staticmap/v5/map?key=IFutcz97fDjINJ1QeUpcmgVDzFLIDfex&center=${lat},${lon}&size=300,300@2x&locations=${lat},${lon}&defaultMarker=marker-red-sm"/>`)
 })
-
 
 async function getStop(stopId) {
     if (stopId == "Union Square-01" || stopId == "Union Square-02") {
@@ -66,3 +56,6 @@ async function getStop(stopId) {
         return data.attributes.platform_name
     }
 }
+
+
+
