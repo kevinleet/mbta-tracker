3e83c53acb188e8946028f12eb8360c8cdfc3230
Kevin Li <kevinli617@gmail.com>
2023-05-20 13:28:48 -0400

    refactored code to separate logic into their respective functions. change api calls to pull station names and heading.

--- script.js
+++ script.js
@@ -21,42 +21,45 @@ function renderMap() {
 }
 renderMap()
 
+
+// Add event listener for the nav bar elements that correspond to train colors. Reset and re-render map and train elements.
 $navList.on('click', 'button', async function() {
     map.remove()
     renderMap()
-    $leftContainer.css("display", "block")
-    $rightContainer.css("grid-column-start", "2")
     $trainList.empty()
     let hex = $(this).attr("hex")
     $leftContainer.css("border", `3px solid ${hex}`)
     let value = $(this).prop("value")
-    let routeFilter = ''
-    switch (value) {
-        case 'green':
-            routeFilter = 'Green-B,Green-C,Green-D,Green-E'
-            break
-        case 'red':
-            routeFilter = 'Red'
-            break
-        case 'orange':
-            routeFilter = 'Orange'
-            break
-        case 'blue':
-            routeFilter = 'Blue'
-            break
-    } 
+    let routeFilter = $(this).attr("route")
     let response = await axios.get(`https://api-v3.mbta.com/vehicles?api_key=${apiKey}&filter[route]=${routeFilter}`)
-    let data = response.data.data
+    parseTrainData(response, value)
+})
+
+// Add event listener to individual train buttons. On click, will center the map location of train selected
+$trainList.on('click', 'button', function() {
+    let lat = $(this).attr("lat")
+    let lon = $(this).attr("lon")
+    map.setView([lat,lon], 16)
+    map.panTo([lat,lon], 16)
+})
+
+// Function that destructures and assigns train data to previously declared variables
+async function parseTrainData(response, value) {
+    let data = await response.data.data
     for (const train of data) {
-        let stop = await getStop(train.relationships.stop.data.id)
         let id = train.id
-        let route = train.relationships.route.data.id
+        let direction_id = train.attributes.direction_id
+        let route_id = train.relationships.route.data.id
+        stop_id = train.relationships.stop?.data?.id
+        let stop_name = await getStopName(stop_id)
+        console.log(stop_id)
+        let current_status = train.attributes.current_status
+        let label = train.attributes.label
         let lat = train.attributes.latitude
         let lon = train.attributes.longitude
+        let direction_name = await getDirectionName(direction_id, route_id)
         let status = ''
-        let currentStatus = train.attributes.current_status
-        L.marker([lat,lon]).addTo(map).bindPopup(`Train ${id}`)
-        switch (currentStatus) {
+        switch (current_status) {
             case 'STOPPED_AT':
                 status += "Stopped At"
                 break
@@ -69,31 +72,37 @@ $navList.on('click', 'button', async function() {
             default:
                 return
         }
-        $trainList.append(`
-        <li>
-            <button class="train-btn ${value}" id="${id}" lat="${lat}" lon="${lon}">
-                ${id}</br>${route}</br>${status}</br>${stop}
-            </button>
-        </li>
-        `)
+        renderTrainButton(value, id, lat, lon, label, status, stop_name, direction_name)
+        L.marker([lat,lon]).addTo(map).bindPopup(`${label} ${direction_name}bound</br>${status} ${stop_name}`)
     }
-})
+}
 
-$trainList.on('click', 'button', function() {
-    let id = $(this).prop("id")
-    let lat = $(this).attr("lat")
-    let lon = $(this).attr("lon")
-    map.setView([lat,lon], 16)
-    map.panTo([lat,lon], 16)
-})
+// Function that renders a train button with arguments to assign button attributes and content
+function renderTrainButton(value, id, lat, lon, label, status, stop_name, direction_name) {
+    $trainList.append(`
+    <li>
+        <button class="train-btn ${value}" label="${label}" lat="${lat}" lon="${lon}">
+            #${label} ${direction_name}bound</br>${status}</br>${stop_name}
+        </button>
+    </li>
+    `)
+}
 
-async function getStop(stopId) {
-    if (stopId == "Union Square-01" || stopId == "Union Square-02") {
+// Function that retrieves direction name when given direction_id and route_id as arguments
+async function getDirectionName(direction_id, route_id) {
+    let response = await axios.get(`https://api-v3.mbta.com/routes?api_key=${apiKey}&filter[id]=${route_id}&filter[direction_id]=${direction_id}`)
+    return response.data.data[0].attributes.direction_names[direction_id]
+}
+
+// Function that retrieves stop name when given stop id as an argument
+async function getStopName(stop_id) {
+    if (stop_id == "Union Square-01" || stop_id == "Union Square-02") {
         return "Union Square"
+    } else if (stop_id == undefined) {
+        return ''
     } else {
-        let response = await axios.get(`https://api-v3.mbta.com/stops/${stopId}?api_key=b2aaa7561cc145a5b412d187b054ba79`)
-        let data = response.data.data
-        return data.attributes.platform_name
+        let response = await axios.get(`https://api-v3.mbta.com/stops/${stop_id}?api_key=${apiKey}`)
+        return response.data.data.attributes.name
     }
 }
 
---
+++ test.js
@@ -0,0 +1,7 @@
+let {id, current_status, label, lat, lon, direction_name, stop_name} = ''
+
+id = 'newstring'
+
+current_status = 'wow'
+console.log(id)
+console.log(current_status)
